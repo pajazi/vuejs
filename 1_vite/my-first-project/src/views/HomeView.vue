57875a0ecc0b94490076e94ab4b31f08488f875c
pavle <zikicpavle@gmail.com>
2022-09-12 10:28:53 +0200

    Composition API Initial Component

--- 1_vite/my-first-project/src/views/HomeView.vue
+++ 1_vite/my-first-project/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
 import TheWelcome from "@/components/TheWelcome.vue";
+import { onMounted, ref } from "vue";
+
+//EXPERIMENTAL $ sign for not using ref.value only using ref = 'new value update'
+const message = $ref("Hello, World!");
+
+onMounted(() => {
+  // message.value = 'qweqwew';
+  message = 'Payche';
+})
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    {{message}}
+
+    <input v-model="message" />
   </main>
 </template>
